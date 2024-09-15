b4b042049cf75cd1116e3055529f6969fad4fbfa
Paul Ellis <paul@movio.co>
2024-09-16 10:35:57 +1200

    Formatter tests

--- app/src/test/java/paufregi/garminfeed/utils/FormatterTest.kt
+++ app/src/test/java/paufregi/garminfeed/utils/FormatterTest.kt
@@ -1,16 +1,23 @@
 package paufregi.garminfeed.utils
 
-import org.junit.Assert.*
-
+import com.google.common.truth.Truth.assertThat
 import org.junit.Test
+import java.time.Instant
+import java.util.Date
+import java.util.Locale
 
 class FormatterTest {
 
     @Test
-    fun getDateTimeForFilename() {
+    fun `Formatter date time for filename`() {
+        val date = Instant.ofEpochMilli(1704057630000) // 2024-01-01 10:20:30
+        assertThat(Formatter.dateTimeForFilename.format(date)).isEqualTo("20240101_102030")
     }
 
     @Test
-    fun getDateTimeForImport() {
+    fun `Formatter date time for importer`() {
+        val date = Date.from(Instant.ofEpochMilli(1704057630000)) // 2024-01-01 10:20:30
+        val result = Formatter.dateTimeForImport(Locale.getDefault()).parse("2024-01-01 10:20:30")
+        assertThat(result).isEqualTo(date)
     }
 }
