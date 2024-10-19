5eebe0bb676ca9f43df2bad82094dc3976a2e959
Paul Ellis <paul@movio.co>
2024-10-19 22:54:17 +1300

    Forcing timezone?

--- app/src/test/kotlin/paufregi/garminfeed/core/utils/FormatterTest.kt
+++ app/src/test/kotlin/paufregi/garminfeed/core/utils/FormatterTest.kt
@@ -3,7 +3,10 @@ package paufregi.garminfeed.core.utils
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 import java.time.Instant
+import java.time.LocalDate
+import java.time.LocalDateTime
 import java.time.ZoneId
+import java.time.ZonedDateTime
 import java.util.Date
 import java.util.Locale
 
@@ -20,7 +23,7 @@ class FormatterTest {
 
     @Test
     fun `Formatter date time for importer`() {
-        val date = Date(1704057630000)  // 2024-01-01 10:20:30
+        val date = Date.from(ZonedDateTime.ofInstant(Instant.ofEpochMilli(1704057630000), zoneId).toInstant())
         val result = Formatter.dateTimeForImport(Locale.ENGLISH).parse("2024-01-01 10:20:30")
         assertThat(result).isEqualTo(date)
     }
