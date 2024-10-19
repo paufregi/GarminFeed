c242fa0d2b9af8b06c535587f5a826e2f5ad8c0f
Paul Ellis <paul@movio.co>
2024-10-19 23:21:39 +1300

    Rollback test

--- app/src/test/kotlin/paufregi/garminfeed/core/utils/FormatterTest.kt
+++ app/src/test/kotlin/paufregi/garminfeed/core/utils/FormatterTest.kt
@@ -3,10 +3,7 @@ package paufregi.garminfeed.core.utils
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 import java.time.Instant
-import java.time.LocalDate
-import java.time.LocalDateTime
 import java.time.ZoneId
-import java.time.ZonedDateTime
 import java.util.Date
 import java.util.Locale
 
@@ -23,7 +20,7 @@ class FormatterTest {
 
     @Test
     fun `Formatter date time for importer`() {
-        val date = Date.from(ZonedDateTime.ofInstant(Instant.ofEpochMilli(1704057630000), zoneId).toInstant())
+        val date = Date.from(Instant.ofEpochMilli(1704057630000))
         val result = Formatter.dateTimeForImport(Locale.ENGLISH).parse("2024-01-01 10:20:30")
         assertThat(result).isEqualTo(date)
     }
