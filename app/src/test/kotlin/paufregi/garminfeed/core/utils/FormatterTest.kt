59122cd47b5c9c491e0b2faf3dba333677050aa0
Paul Ellis <paul@movio.co>
2024-10-19 22:14:51 +1300

    WIP

--- app/src/test/kotlin/paufregi/garminfeed/core/utils/FormatterTest.kt
+++ app/src/test/kotlin/paufregi/garminfeed/core/utils/FormatterTest.kt
@@ -20,6 +20,7 @@ class FormatterTest {
 
     @Test
     fun `Formatter date time for importer`() {
+        Locale.setDefault(Locale.ENGLISH)
         val date = Date.from(Instant.ofEpochMilli(1704057630000)) // 2024-01-01 10:20:30
         val result = Formatter.dateTimeForImport(Locale.ENGLISH).parse("2024-01-01 10:20:30")
         assertThat(result).isEqualTo(date)
--- app/src/test/kotlin/paufregi/garminfeed/core/utils/RenphoReaderTest.kt
+++ app/src/test/kotlin/paufregi/garminfeed/core/utils/RenphoReaderTest.kt
@@ -6,12 +6,14 @@ import org.junit.Test
 import paufregi.garminfeed.core.models.Weight
 import java.time.Instant
 import java.util.Date
+import java.util.Locale
 
 
 class RenphoReaderTest {
 
     @Test
     fun `Convert Renpho CSV to list of weights`() {
+        Locale.setDefault(Locale.ENGLISH)
         val csvText = """
             Time of Measurement,Weight(kg),BMI,Body Fat(%),Fat-Free Mass(kg),Subcutaneous Fat(%),Visceral Fat,Body Water(%),Skeletal Muscle(%),Muscle Mass(kg),Bone Mass(kg),Protein(%),BMR(kcal),Metabolic Age,Optimal weight(kg),Target to optimal weight(kg),Target to optimal fat mass(kg),Target to optimal muscle mass(kg),Body Type,Remarks
             2024-01-01 10:20:30,76.15,23.8,23.2,58.48,20.9,7.0,55.4,49.5,55.59,2.89,17.5,1618,35,,,,,,
