efffe2995958e50617aca02e074db484f16a9a82
Paul Ellis <paul@movio.co>
2024-10-27 22:46:56 +1300

    Add use cases for quickEdit + little clean up

--- app/src/main/kotlin/paufregi/garminfeed/core/usecases/UpdateActivityUseCase.kt
+++ app/src/main/kotlin/paufregi/garminfeed/core/usecases/UpdateActivityUseCase.kt
@@ -2,20 +2,19 @@ package paufregi.garminfeed.core.usecases
 
 import android.util.Log
 import paufregi.garminfeed.core.models.Activity
-import paufregi.garminfeed.core.models.Credential
 import paufregi.garminfeed.core.models.Profile
 import paufregi.garminfeed.core.models.Result
 import paufregi.garminfeed.data.repository.GarminRepository
 import javax.inject.Inject
 
-class SaveActivityUseCase @Inject constructor (private val garminRepository: GarminRepository) {
+class UpdateActivityUseCase @Inject constructor (private val garminRepository: GarminRepository) {
     suspend operator fun invoke(
         activity: Activity?,
         profile: Profile?
-    ):Result<Unit> {
-        return if (activity != null && profile != null)
-            Result.Success(Unit)
-        else
-            Result.Failure("Invalid values")
+    ):Result<Unit>{
+        if (activity != null && profile != null) {
+            return garminRepository.updateActivity(activity, profile)
+        }
+        return Result.Failure("Validation error")
     }
 }
--- app/src/test/kotlin/paufregi/garminfeed/core/usecases/GetProfilesUseCaseTest.kt
+++ app/src/test/kotlin/paufregi/garminfeed/core/usecases/GetProfilesUseCaseTest.kt
@@ -1,15 +1,36 @@
 package paufregi.garminfeed.core.usecases
 
+import com.google.common.truth.Truth.assertThat
+import io.mockk.clearAllMocks
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
 import paufregi.garminfeed.core.models.ActivityType
 import paufregi.garminfeed.core.models.Course
 import paufregi.garminfeed.core.models.EventType
 import paufregi.garminfeed.core.models.Profile
 import paufregi.garminfeed.data.repository.GarminRepository
-import javax.inject.Inject
+import kotlin.math.exp
 
-class GetProfilesUseCase @Inject constructor (private val garminRepository: GarminRepository) {
-    operator fun invoke(): List<Profile> {
-        return listOf(
+class GetProfilesUseCaseTest{
+    private val repo = mockk<GarminRepository>()
+    private lateinit var useCase: GetProfilesUseCase
+
+    @Before
+    fun setup(){
+        useCase = GetProfilesUseCase(repo)
+    }
+
+    @After
+    fun tearDown(){
+        clearAllMocks()
+    }
+
+    @Test
+    fun `Get profiles use-case`() = runTest{
+        val expected = listOf(
             Profile(
                 Course.home.name,
                 EventType.transportation,
@@ -23,5 +44,9 @@ class GetProfilesUseCase @Inject constructor (private val garminRepository: Garm
                 Course.work,
                 500),
         )
+
+        val res = useCase()
+
+        assertThat(res).isEqualTo(expected)
     }
 }
