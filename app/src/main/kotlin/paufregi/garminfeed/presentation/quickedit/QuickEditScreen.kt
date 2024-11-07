3ab80fcef5169e018f41babde96bfc134407f90a
Paul Ellis <paul@movio.co>
2024-11-07 17:03:27 +1300

    WIP - radio buttons and feel

--- app/src/main/kotlin/paufregi/garminfeed/presentation/quickedit/QuickEditScreen.kt
+++ app/src/main/kotlin/paufregi/garminfeed/presentation/quickedit/QuickEditScreen.kt
@@ -1,5 +1,6 @@
 package paufregi.garminfeed.presentation.quickedit
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,20 +10,24 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import paufregi.garminfeed.core.models.ActivityType
 import paufregi.garminfeed.presentation.ui.components.Button
 import paufregi.garminfeed.presentation.ui.components.CustomSlider
 import paufregi.garminfeed.presentation.ui.components.Dropdown
@@ -62,20 +67,28 @@ internal fun QuickEditScreen(
 
                 Dropdown(
                     label = { Text("Profile") },
-                    selected = state.selectedActivity?.toDropdownItem { },
+                    selected = state.selectedProfile?.toDropdownItem { },
                     items = state.availableProfiles.map { it.toDropdownItem { onEvent(QuickEditEvent.SelectProfile(it)) } }
                 )
 
-                Slider(
-                    value = state.selectedEffort,
-                    onValueChange = { onEvent(QuickEditEvent.SelectEffort(it)) },
-                    valueRange = 0f..10f,
-                    steps = 9,
-                    interactionSource = interactionSource,
-                    track = CustomSlider.track,
-                    thumb = CustomSlider.thumb(interactionSource)
-                )
-                TextEffort(state.selectedEffort)
+                if(state.selectedActivity?.type == ActivityType.Cycling) {
+                    Column{
+                        Slider(
+                            value = state.selectedEffort,
+                            onValueChange = { onEvent(QuickEditEvent.SelectEffort(it)) },
+                            valueRange = 0f..10f,
+                            steps = 9,
+                            interactionSource = interactionSource,
+                            track = CustomSlider.track,
+                            thumb = CustomSlider.thumb(interactionSource)
+                        )
+                        TextEffort(
+                            state.selectedEffort,
+                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                        )
+                    }
+                    RadioButton()
+                }
 
                 Row(modifier = Modifier.fillMaxWidth()) {
                     Button(text = "Cancel", onClick = { nav.navigateUp() })
--- app/src/main/kotlin/paufregi/garminfeed/presentation/ui/components/TextEffort.kt
+++ app/src/main/kotlin/paufregi/garminfeed/presentation/ui/components/TextEffort.kt
@@ -2,19 +2,20 @@ package paufregi.garminfeed.presentation.ui.components
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
-fun TextEffort(effort: Float) {
+fun TextEffort(effort: Float, modifier: Modifier = Modifier) {
     when(effort) {
-        0f -> Text("None selected")
-        1f -> Text("${effort.toInt()} - Very light")
-        2f -> Text("${effort.toInt()} - Light")
-        3f -> Text("${effort.toInt()} - Moderate")
-        4f -> Text("${effort.toInt()} - Somewhat Hard")
-        5f, 6f -> Text("${effort.toInt()} - Hard")
-        7f, 8f -> Text("${effort.toInt()} - Very Hard")
-        9f -> Text("${effort.toInt()} - Extremely Hard")
-        10f -> Text("${effort.toInt()} - Maximum")
-        else -> Text("${effort.toInt()} - Whet!?")
+        0f -> Text("None selected", modifier = modifier)
+        1f -> Text("${effort.toInt()} - Very light", modifier = modifier)
+        2f -> Text("${effort.toInt()} - Light", modifier = modifier)
+        3f -> Text("${effort.toInt()} - Moderate", modifier = modifier)
+        4f -> Text("${effort.toInt()} - Somewhat Hard", modifier = modifier)
+        5f, 6f -> Text("${effort.toInt()} - Hard", modifier = modifier)
+        7f, 8f -> Text("${effort.toInt()} - Very Hard", modifier = modifier)
+        9f -> Text("${effort.toInt()} - Extremely Hard", modifier = modifier)
+        10f -> Text("${effort.toInt()} - Maximum", modifier = modifier)
+        else -> Text("${effort.toInt()} - What!?", modifier = modifier)
     }
 }
