4c0bbe46688a4a5c20e74ccc8f2f588c392d0bd3
Paul Ellis <paul@movio.co>
2024-11-18 17:12:21 +1300

    This function has been moved into the NavBar component definition

--- app/src/main/kotlin/paufregi/connectfeed/presentation/utils/Navigation.kt
+++
@@ -1,7 +0,0 @@
-package paufregi.connectfeed.presentation.utils
-
-import androidx.compose.ui.graphics.vector.ImageVector
-import paufregi.connectfeed.presentation.main.Routes
-
-data class NavItem(val route: Routes, val label: String, val icons: ImageVector)
-
