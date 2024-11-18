2cb09a19c9899992c81ae76b9a94a5f8bfc11b4b
Paul Ellis <paul@movio.co>
2024-11-18 17:15:24 +1300

    Add a ViewModel to the main to check when the initial setup is done

--- app/src/main/kotlin/paufregi/connectfeed/presentation/main/MainActivity.kt
+++ app/src/main/kotlin/paufregi/connectfeed/presentation/main/MainActivity.kt
@@ -1,78 +1,79 @@
 package paufregi.connectfeed.presentation.main
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.Tune
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
-import paufregi.connectfeed.presentation.home.HomeScreen
+import paufregi.connectfeed.presentation.Routes
 import paufregi.connectfeed.presentation.profiles.ProfilesScreen
+import paufregi.connectfeed.presentation.quickedit.QuickEditScreen
 import paufregi.connectfeed.presentation.settings.SettingsScreen
+import paufregi.connectfeed.presentation.setup.SetupScreen
+import paufregi.connectfeed.presentation.ui.components.Loading
+import paufregi.connectfeed.presentation.ui.components.NavBar
+import paufregi.connectfeed.presentation.ui.components.NavItem
 import paufregi.connectfeed.presentation.ui.theme.Theme
 import paufregi.connectfeed.presentation.ui.components.SnackbarObserver
-import paufregi.connectfeed.presentation.utils.NavItem
+import kotlin.getValue
 
 @AndroidEntryPoint
 @ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            val scope = rememberCoroutineScope()
-            val snackState = remember { SnackbarHostState() }
-            val nav = rememberNavController()
+            setContent {
+                val scope = rememberCoroutineScope()
+                val snackState = remember { SnackbarHostState() }
+                val nav = rememberNavController()
 
-            val navItems = listOf(
-                NavItem(Routes.Home, "Home", Icons.Default.Home),
-                NavItem(Routes.Profiles, "Profiles", Icons.Default.Tune),
-                NavItem(Routes.Settings, "Settings", Icons.Default.Settings),
-            )
+                Theme {
+                    val setupDone by viewModel.state.collectAsStateWithLifecycle()
 
-            Log.i("MainActivity", "currentDestination: ${nav.currentDestination}")
-            Log.i("MainActivity", "currentDestination: ${nav.currentDestination?.route}")
-
-            Theme {
-                SnackbarObserver(snackState, scope)
-                Scaffold(
-                    modifier = Modifier.fillMaxSize(),
-                    snackbarHost = { SnackbarHost(hostState = snackState) },
-                    bottomBar = { NavigationBar {
-                        navItems.forEach {
-                            NavigationBarItem(
-                                selected = false, //TODO: Implement selected
-                                onClick = { nav.navigate(it.route) },
-                                label = { Text(it.label) },
-                                alwaysShowLabel = true,
-                                icon = { Icon(it.icons, null) }
-                            )
-                        }
-                    } }
-                ) { pd ->
+                    SnackbarObserver(snackState, scope)
+                    Scaffold(
+                        modifier = Modifier.fillMaxSize(),
+                        snackbarHost = { SnackbarHost(hostState = snackState) },
+                        bottomBar = { NavBar(
+                            nav = nav,
+                            navItems = listOf(
+                                NavItem(Routes.Home, "Home", Icons.Default.Home),
+                                NavItem(Routes.Profiles, "Profiles", Icons.Default.Tune),
+                            NavItem(Routes.Settings, "Settings", Icons.Default.Settings),
+                        ),
+                    ) }
+                ) { pv ->
                     NavHost(navController = nav, startDestination = Routes.Home) {
-                        composable<Routes.Home> { HomeScreen(pd, nav) }
-                        composable<Routes.Settings> { SettingsScreen(pd, nav) }
-                        composable<Routes.Profiles> { ProfilesScreen(pd, nav) }
+                        composable<Routes.Home> {
+                            when (setupDone)   {
+                                true -> QuickEditScreen(pv)
+                                false -> SetupScreen(pv)
+                                else -> Loading(pv)
+                            }
+                        }
+                        composable<Routes.Profiles> { ProfilesScreen(pv) }
+                        composable<Routes.Settings> { SettingsScreen(pv) }
                     }
                 }
             }
---
+++ app/src/main/kotlin/paufregi/connectfeed/presentation/main/MainViewModel.kt
@@ -0,0 +1,18 @@
+package paufregi.connectfeed.presentation.main
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
+import paufregi.connectfeed.core.usecases.GetSetupDoneUseCase
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    getSetupDoneUseCase: GetSetupDoneUseCase
+) : ViewModel() {
+
+    val state = getSetupDoneUseCase()
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000L), null)
+}
