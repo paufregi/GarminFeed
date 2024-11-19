153fc4d78a652c85153050956eae703a57febd20
Paul Ellis <paul@movio.co>
2024-11-19 15:19:19 +1300

    Remve ProcessState

--- app/src/main/kotlin/paufregi/connectfeed/presentation/utils/ProcessState.kt
+++
@@ -1,8 +0,0 @@
-package paufregi.connectfeed.presentation.utils
-
-sealed interface ProcessState {
-    object Idle : ProcessState
-    object Processing : ProcessState
-    object Success : ProcessState
-    object Failure : ProcessState
-}
