63519219b32fc8ed3f51bddf585536b3b23a7046
Paul Ellis <paul@movio.co>
2024-11-24 21:15:06 +1300

    More activity types, add order and fix icons

--- app/src/main/kotlin/paufregi/connectfeed/core/models/ActivityType.kt
+++ app/src/main/kotlin/paufregi/connectfeed/core/models/ActivityType.kt
@@ -1,17 +1,20 @@
 package paufregi.connectfeed.core.models
 
-sealed class ActivityType(val name: String) {
-    object Running : ActivityType("Running")
-    object Cycling : ActivityType("Cycling")
-    object Strength : ActivityType("Strength")
-    object Any : ActivityType("Any")
-    object Unknown : ActivityType("Unknown")
+sealed class ActivityType(val name: String, val order: Int) {
+    object Any : ActivityType("Any", 1)
+    object Running : ActivityType("Running", 2)
+    object TrailRunning : ActivityType("Trail Running", 3)
+    object Cycling : ActivityType("Cycling", 4)
+    object Strength : ActivityType("Strength", 5)
+    object Unknown : ActivityType("Unknown", 100)
 
     companion object {
         fun fromName(name: String): ActivityType = when (name.lowercase()) {
-            "running" -> Running
-            "Cycling" -> Cycling
             "any" -> Any
+            "running" -> Running
+            "trail_running" -> TrailRunning
+            "Cycling" -> Cycling
+            "strength" -> Strength
             else -> Unknown
         }
     }
--- app/src/main/kotlin/paufregi/connectfeed/presentation/ui/components/ActivityIcon.kt
+++ app/src/main/kotlin/paufregi/connectfeed/presentation/ui/components/ActivityIcon.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.icons.automirrored.filled.DirectionsBike
 import androidx.compose.material.icons.automirrored.filled.DirectionsRun
 import androidx.compose.material.icons.filled.Accessibility
 import androidx.compose.material.icons.filled.FitnessCenter
+import androidx.compose.material.icons.filled.Hiking
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
@@ -20,10 +21,11 @@ fun ActivityIcon(
     @PreviewParameter(ActivityTypePreview ::class) activityType: ActivityType?
 ) {
     when (activityType) {
+        is ActivityType.Any -> Icon(Icons.Default.Accessibility, null)
         is ActivityType.Running -> Icon(Icons.AutoMirrored.Default.DirectionsRun, null)
+        is ActivityType.TrailRunning -> Icon(Icons.Default.Hiking, null)
         is ActivityType.Cycling -> Icon(Icons.AutoMirrored.Default.DirectionsBike, null)
-        is ActivityType.Strength -> Icon(Icons.AutoMirrored.Default.DirectionsBike, null)
-        is ActivityType.Any -> Icon(Icons.Default.FitnessCenter, null)
+        is ActivityType.Strength -> Icon(Icons.Default.FitnessCenter, null)
         else -> null
     }
 }
