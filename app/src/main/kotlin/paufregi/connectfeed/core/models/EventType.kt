7b1a5b0777c707e3578593e5f82b251ca18e3cb5
Paul Ellis <paul@movio.co>
2024-11-27 19:55:11 +1300

    Remove companion object

--- app/src/main/kotlin/paufregi/connectfeed/core/models/EventType.kt
+++ app/src/main/kotlin/paufregi/connectfeed/core/models/EventType.kt
@@ -3,9 +3,4 @@ package paufregi.connectfeed.core.models
 data class EventType(
     val id: Long,
     val name: String,
-) {
-    companion object {
-        val transportation = EventType(5, "Transportation")
-        val training = EventType(4, "Training")
-    }
-}
+)
