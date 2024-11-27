c7b86a4718ebe22edd55f31d01469f130c84e415
Paul Ellis <paul@movio.co>
2024-11-27 19:55:00 +1300

    Fix fromName function for ActivityType

--- app/src/main/kotlin/paufregi/connectfeed/core/models/ActivityType.kt
+++ app/src/main/kotlin/paufregi/connectfeed/core/models/ActivityType.kt
@@ -1,20 +1,20 @@
 package paufregi.connectfeed.core.models
 
 sealed class ActivityType(val name: String, val order: Int) {
-    object Any : ActivityType("Any", 1)
-    object Running : ActivityType("Running", 2)
-    object TrailRunning : ActivityType("Trail Running", 3)
-    object Cycling : ActivityType("Cycling", 4)
-    object Strength : ActivityType("Strength", 5)
-    object Unknown : ActivityType("Unknown", 100)
+    data object Any : ActivityType("Any", 1)
+    data object Running : ActivityType("Running", 2)
+    data object TrailRunning : ActivityType("Trail Running", 3)
+    data object Cycling : ActivityType("Cycling", 4)
+    data object Strength : ActivityType("Strength", 5)
+    data object Unknown : ActivityType("Unknown", 100)
 
     companion object {
-        fun fromName(name: String): ActivityType = when (name.lowercase()) {
+        fun fromName(name: String?): ActivityType = when (name?.lowercase()) {
             "any" -> Any
             "running" -> Running
-            "trail_running" -> TrailRunning
+            "trail_running", "trail running" -> TrailRunning
             "cycling" -> Cycling
-            "strength_training" -> Strength
+            "strength_training", "strength" -> Strength
             else -> Unknown
         }
     }
