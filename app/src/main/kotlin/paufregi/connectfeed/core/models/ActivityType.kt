20ddc22ade6a7b8cf438176ae7de1447f56c66da
Paul Ellis <paul@movio.co>
2024-11-29 09:39:25 +1300

    Move convertion function into converter class

--- app/src/main/kotlin/paufregi/connectfeed/core/models/ActivityType.kt
+++ app/src/main/kotlin/paufregi/connectfeed/core/models/ActivityType.kt
@@ -7,15 +7,4 @@ sealed class ActivityType(val name: String, val order: Int) {
     data object Cycling : ActivityType("Cycling", 4)
     data object Strength : ActivityType("Strength", 5)
     data object Unknown : ActivityType("Unknown", 100)
-
-    companion object {
-        fun fromName(name: String?): ActivityType = when (name?.lowercase()) {
-            "any" -> Any
-            "running" -> Running
-            "trail_running", "trail running" -> TrailRunning
-            "cycling" -> Cycling
-            "strength_training", "strength" -> Strength
-            else -> Unknown
-        }
-    }
 }
--- app/src/main/kotlin/paufregi/connectfeed/data/database/coverters/ActivityTypeConverter.kt
+++ app/src/main/kotlin/paufregi/connectfeed/data/database/coverters/ActivityTypeConverter.kt
@@ -2,10 +2,22 @@ package paufregi.connectfeed.data.database.coverters
 
 import androidx.room.TypeConverter
 import paufregi.connectfeed.core.models.ActivityType
+import paufregi.connectfeed.core.models.ActivityType.Cycling
+import paufregi.connectfeed.core.models.ActivityType.Running
+import paufregi.connectfeed.core.models.ActivityType.Strength
+import paufregi.connectfeed.core.models.ActivityType.TrailRunning
+import paufregi.connectfeed.core.models.ActivityType.Unknown
 
 class ActivityTypeConverter {
     @TypeConverter
-    fun fromString(type: String?): ActivityType = ActivityType.fromName(type)
+    fun fromString(type: String?): ActivityType = when (type?.lowercase()) {
+        "any" -> ActivityType.Any
+        "running" -> Running
+        "trail_running", "trail running" -> TrailRunning
+        "cycling" -> Cycling
+        "strength_training", "strength" -> Strength
+        else -> Unknown
+    }
 
     @TypeConverter
     fun toString(type: ActivityType?): String? = type?.name
