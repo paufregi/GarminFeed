2ca0cf719c27c95c6aef11a624fd167d5ab4622f
Paul Ellis <paul@movio.co>
2024-12-18 16:44:23 +1300

    Remove setting screen

--- app/src/main/kotlin/paufregi/connectfeed/presentation/settings/SettingsEvent.kt
+++
@@ -1,9 +0,0 @@
-package paufregi.connectfeed.presentation.settings
-
-
-sealed interface SettingsEvent {
-    data class SetUsername(val username: String): SettingsEvent
-    data class SetPassword(val password: String): SettingsEvent
-    data class SetShowPassword(val showPassword: Boolean): SettingsEvent
-    data object Save: SettingsEvent
-}
--- app/src/main/kotlin/paufregi/connectfeed/presentation/settings/SettingsState.kt
+++
@@ -1,8 +0,0 @@
-package paufregi.connectfeed.presentation.settings
-
-import paufregi.connectfeed.core.models.Credential
-
-data class SettingsState(
-    val credential: Credential = Credential(),
-    val showPassword: Boolean = false,
-)
