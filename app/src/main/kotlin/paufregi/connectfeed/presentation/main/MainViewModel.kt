1f36f570c83880ce9f28f2d7ec16bd38c356ca35
Paul Ellis <paul@movio.co>
2024-12-22 00:08:09 +1300

    Combine state and isLogged flow properly

--- app/src/main/kotlin/paufregi/connectfeed/presentation/main/MainViewModel.kt
+++ app/src/main/kotlin/paufregi/connectfeed/presentation/main/MainViewModel.kt
@@ -5,28 +5,21 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 import paufregi.connectfeed.core.usecases.IsLoggedIn
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val isLoggedIn: IsLoggedIn
+    isLoggedIn: IsLoggedIn
 ) : ViewModel() {
 
     private val _state = MutableStateFlow(MainState())
 
-    val state = _state
-        .onStart { load() }
+    val state = combine(_state, isLoggedIn()) { state, logged -> state.copy(loggedIn = logged) }
         .stateIn(viewModelScope, SharingStarted.WhileSubscribed(1000L), MainState())
-
-    private fun load() = viewModelScope.launch {
-        isLoggedIn().map { loggedIn -> _state.update { it.copy(loggedIn = loggedIn) } }
-    }
 
     fun showLogin() = _state.update { it.copy(showLogin = true) }
     fun hideLogin() = _state.update { it.copy(showLogin = false) }
